a061bf1489ff0205295ba3cd7b7a4f7c980547df
ViaMay <vi-113@yandex.ru>
2021-10-25 00:54:52 +0500

    add data

---
+++ app/src/main/java/com/whate_dragon/shoping_list/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.whate_dragon.shoping_list.data
+
+import com.whate_dragon.shoping_list.domain.ShopItem
+import com.whate_dragon.shoping_list.domain.ShopListRepository
+import java.lang.RuntimeException
+
+object ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID) {
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopItem(shopItemId: Int): ShopItem {
+        return  shopList.find {
+            it.id == shopItemId
+        } ?: throw RuntimeException("Element with id $shopItemId not found")
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return  shopList.toList()
+    }
+}
